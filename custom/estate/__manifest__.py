c7fb4f76f5b91a1c92eaf52947589aaff1618c01
PrincessBenido <princess.benido@linserv.segit config --global --listgit config --global user.email princess.benido@linserv.se>
2022-08-10 14:55:29 +0300

    first commit

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -16,19 +16,24 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sales',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account'],
+    'depends': [
+        'base',
+        'account'
+    ],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'security/security.xml',
         'views/estate_prop_view.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/res_users_views.xml',
+
         'views/estate_menus.xml',
     ],
     # only loaded in demonstration mode
